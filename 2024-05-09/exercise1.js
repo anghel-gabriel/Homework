bf0ed4dc5caed28c253424ec7a7bc6bb2c4f6da3
Anghel <anghel.ergosum@gmail.com>
2023-05-25 17:25:46 +0300

    Extra methods

--- 2024-05-09/exercise1.js
+++ 2024-05-09/exercise1.js
@@ -1,32 +1,32 @@
 /* Declararea persoanelor */
 const persoana1 = {
-	nume: "Duhanes",
-	prenume: "Andrei",
+	nume: 'Duhanes',
+	prenume: 'Andrei',
 	anulNasterii: 1990,
-	resedinta: "Cluj-Napoca",
-	abilitati: ["HTML", "CSS", "Javascript", "Angular", "SQL"],
+	resedinta: 'Cluj-Napoca',
+	abilitati: ['HTML', 'CSS', 'Javascript', 'Angular', 'SQL'],
 	experienta: 5,
-	descriere: "Sunt o persoana dornica sa invete tehnologii noi.",
+	descriere: 'Sunt o persoana dornica sa invete tehnologii noi.',
 };
 
 const persoana2 = {
-	nume: "Anghel",
-	prenume: "Gabriel",
+	nume: 'Anghel',
+	prenume: 'Gabriel',
 	anulNasterii: 1997,
-	resedinta: "Bucuresti",
-	abilitati: ["HTML", "CSS"],
+	resedinta: 'Bucuresti',
+	abilitati: ['HTML', 'CSS'],
 	experienta: 0,
-	descriere: "Vreau sa ma angajez ca front-end developer",
+	descriere: 'Vreau sa ma angajez ca front-end developer',
 };
 
 const persoana3 = {
-	nume: "Numeexemplu",
-	prenume: "Prenumeexemplu",
+	nume: 'Numeexemplu',
+	prenume: 'Prenumeexemplu',
 	anulNasterii: 2002,
-	resedinta: "Resedintaexemplu",
-	abilitati: ["CSS", "HTML", "React"],
+	resedinta: 'Resedintaexemplu',
+	abilitati: ['React', 'Angular', 'Node'],
 	experienta: 7,
-	descriere: "Sunt un element de şir",
+	descriere: 'Sunt un element de şir',
 };
 
 /* Declararea listei de persoane */
@@ -36,13 +36,13 @@ const listaMea = [persoana3, persoana1, persoana2];
 const persoaneFunction = function (lista) {
 	return {
 		listOutTheFullNames: function () {
-			for (i = 0; i < lista.length; i++) {
+			for (let i = 0; i < lista.length; i++) {
 				console.log(`${lista[i].nume},  ${lista[i].prenume}`);
 				/* 				console.log(lista[i].nume + ", " + lista[i].prenume) */
 			}
 		},
 		listOutTheEntireInformation: function () {
-			for (i = 0; i < lista.length; i++) {
+			for (let i = 0; i < lista.length; i++) {
 				console.log(`${lista[i].nume}, ${lista[i].prenume}, ${lista[i].anulNasterii}, ${lista[i].resedinta}, ${lista[i].abilitati}, ${lista[i].experienta}, ${lista[i].descriere}`);
 			}
 		},
@@ -62,12 +62,46 @@ const persoaneFunction = function (lista) {
 		},
 		getPeopleByYearsOfExperience: function () {
 			const listaSeniori = [];
-			for (i = 0; i < lista.length; i++) {
+			for (let i = 0; i < lista.length; i++) {
 				if (lista[i].experienta >= 4) {
 					listaSeniori.push(`${lista[i].nume} ${lista[i].prenume}`);
 				}
 			}
 			return listaSeniori;
+		},
+		getPeopleByExactSkillsSet: function (skillsArr) {
+			let peopleWithNecessarySkills = [];
+			for (let i = 0; i < lista.length; i++) {
+				if (lista[i].abilitati.includes(...skillsArr)) {
+					peopleWithNecessarySkills.push(lista[i]);
+				}
+			}
+			return peopleWithNecessarySkills;
+		},
+		getPeopleBySomeSkillSet: function (skillsArr) {
+			console.log(skillsArr);
+			let peopleWithSomeSkills = [];
+			for (let i = 0; i < lista.length; i++) {
+				for (let j = 0; j < skillsArr.length; j++) {
+					if (lista[i].abilitati.includes(skillsArr[j]) && !peopleWithSomeSkills.includes(lista[i])) {
+						peopleWithSomeSkills.push(lista[i]);
+					}
+				}
+			}
+			return peopleWithSomeSkills;
+		},
+		getHtmlContent: function () {
+			listaMea.map((persoanaObj) => {
+				document.querySelector('body').innerHTML += `
+    			<div style="margin-top: 60px">
+						<h1>Nume complet: ${persoanaObj.nume} ${persoanaObj.prenume}</h1>
+						<h2>Anul naşterii: ${persoanaObj.anulNasterii}</h2>
+						<h3>Reşedinţă: ${persoanaObj.resedinta}</h3>
+						<h4>Abilităţi: ${persoanaObj.abilitati}</h4>
+						<h5>Experienţă: ${persoanaObj.experienta} ani</h5>
+						<p> Descriere: "${persoanaObj.descriere}"</p>
+   				</div>`;
+			});
 		},
 	};
 };
@@ -76,5 +110,8 @@ const persoaneFunction = function (lista) {
 const listaPersoaneFunc = persoaneFunction(listaMea);
 listaPersoaneFunc.listOutTheFullNames();
 listaPersoaneFunc.listOutTheEntireInformation();
-console.log(listaPersoaneFunc.getPeopleByAlphabeticalOrder());
-console.log(listaPersoaneFunc.getPeopleByYearsOfExperience());
+console.log('Persoane ordine alfabetica', listaPersoaneFunc.getPeopleByAlphabeticalOrder());
+console.log('Programatori seniori', listaPersoaneFunc.getPeopleByYearsOfExperience());
+console.log('Persoane cu exact abilitati', listaPersoaneFunc.getPeopleByExactSkillsSet(['HTML', 'CSS']));
+console.log('Persoane cu una dintre abilitati', listaPersoaneFunc.getPeopleByExactSkillsSet(['React', 'Angular', 'CSS']));
+listaPersoaneFunc.getHtmlContent();
