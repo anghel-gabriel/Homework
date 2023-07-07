4b9a85d33be17633347ebd0c606a721a084a6845
Anghel <anghel.ergosum@gmail.com>
2023-07-07 18:09:08 +0300

    Deleted the right padding

--- 2023-07-07/exercise6/exercise6.js
+++ 2023-07-07/exercise6/exercise6.js
@@ -4,7 +4,9 @@ function creazaPatrat(lungimeLatura) {
 	for (let r = 0; r < lungimeLatura; r++) {
 		let linieDesenata = '';
 		for (let c = 0; c < lungimeLatura; c++) {
-			if (r === 0 || r === lungimeLatura - 1 || c === 0 || c === lungimeLatura - 1) {
+			if (c === lungimeLatura - 1) {
+				linieDesenata += '*';
+			} else if (r === 0 || r === lungimeLatura - 1 || c === 0 || c === lungimeLatura - 1) {
 				linieDesenata += '*  ';
 			} else {
 				linieDesenata += '   ';
